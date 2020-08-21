9e0effba4f30b3b17e9a1a4f72c1460db15c1ac1
Henry Chung <chungthehao@gmail.com>
2020-08-21 10:50:55 +0700

    Responding With Data

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http'); // node core module
 
+const todos = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+  { id: 3, text: 'Todo Three' },
+];
+
 const server = http.createServer(({ headers, url, method }, res) => {
-  console.log(headers, url, method);
-  res.end();
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'Node.js');
+
+  //   res.write('<h1>Hi Henry!</h1>');
+  //   res.write('<h2>You are awesome!</h2>');
+
+  // res.end(); // Không có thì bên client, quay vòng vòng hoài.
+  res.end(JSON.stringify({ success: true, data: todos })); // có thể truyền cái cần trả về bằng param trực tiếp trong hàm end luôn.
 });
 
 const PORT = 5000;
